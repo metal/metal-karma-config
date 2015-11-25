1f236b9d33e6cdc115367c9b3b64ece006f55bb9
Maira Bello <maira.araujo@liferay.com>
2015-11-25 17:57:48 -0300

    Adds default karma config

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var babelPresetMetal = require('babel-preset-metal');
+var karmaBabelPreprocessor = require('karma-babel-preprocessor');
+var karmaChai = require('karma-chai');
+var karmaChromeLauncher = require('karma-chrome-launcher');
+var karmaCommonJs = require('karma-commonjs');
+var karmaMocha = require('karma-mocha');
+var karmaSinon = require('karma-sinon');
+var karmaSourceMapSupport = require('karma-source-map-support');
+
+var babelOptions = {
+  presets: [babelPresetMetal],
+  sourceMap: 'both'
+};
+
+module.exports = function (config) {
+  config.set({
+  	plugins: [
+  		karmaBabelPreprocessor,
+    	karmaChai,
+  		karmaChromeLauncher,
+    	karmaCommonJs,
+    	karmaMocha,
+    	karmaSourceMapSupport,
+    	karmaSinon
+  	],
+
+    frameworks: ['mocha', 'chai', 'sinon', 'source-map-support', 'commonjs'],
+
+    files: [
+      'bower_components/soyutils/soyutils.js',
+			'bower_components/metal*/src/**/*.js',
+			'bower_components/crystal*/src/**/*.js',
+			'bower_components/steel*/src/**/*.js',
+      'src/**/*.js',
+      'test/**/*.js'
+    ],
+
+    preprocessors: {
+			'src/**/*.js': ['babel', 'commonjs'],
+			'bower_components/metal*/**/*.js': ['babel', 'commonjs'],
+			'bower_components/crystal*/**/*.js': ['babel', 'commonjs'],
+			'bower_components/steel*/**/*.js': ['babel', 'commonjs'],
+      'test/**/*.js': ['babel', 'commonjs']
+    },
+
+    browsers: ['Chrome'],
+
+    babelPreprocessor: {options: babelOptions}
+  });
+};
